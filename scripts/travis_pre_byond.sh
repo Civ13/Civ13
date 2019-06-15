d36f2f90fb2b87486968e500ee22731ea6cc3a6a
cariosecus <cariosecus@gmail.com>
2019-06-15 04:54:33 +0100

    hm

--- scripts/travis_pre_byond.sh
+++ scripts/travis_pre_byond.sh
@@ -2,29 +2,24 @@
 set -e
 
 
-if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${BYOND_MAJOR}.${BYOND_MINOR}" $HOME/BYOND/version.txt;
+echo "Setting up BYOND."
+rm -rf "$HOME/BYOND"
+mkdir -p "$HOME/BYOND"
+cd "$HOME/BYOND"
+curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+unzip byond.zip
+rm byond.zip
+cd byond
+make install
+echo "Compiling the dme..."
+retval=1
+source $HOME/BYOND/byond/bin/byondsetup
+DreamMaker -max_errors 0 $HOME/Civ13-SS13/Civ13/civ13.dme | tee result.log
+retval=$?
+if ! grep '\- 0 errors, 0 warnings' result.log
 then
-	echo "Using cached directory."
-else
-	echo "Setting up BYOND."
-	rm -rf "$HOME/BYOND"
-	mkdir -p "$HOME/BYOND"
-	cd "$HOME/BYOND"
-	curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
-	unzip byond.zip
-	rm byond.zip
-	cd byond
-	make here
-	echo "$BYOND_MAJOR.$BYOND_MINOR" > "$HOME/BYOND/version.txt"
-	echo "Compiling the dme..."
-	cd /home/travis/BYOND/byond/bin
-	DreamMaker -max_errors 0 $HOME/Civ13-SS13/Civ13/civ13.dme | tee result.log
-	retval=$?
-	if ! grep '\- 0 errors, 0 warnings' result.log
-	then
-		retval=1 #hard fail, due to warnings or errors
-		echo "Failed to compile!"
-		exit 1
-		fi
-	fi;
+	retval=1 #hard fail, due to warnings or errors
+	echo "Failed to compile!"
+	exit 1
+	fi
 fi;
