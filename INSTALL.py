97b5a250f6c9e71578150de57e2ca93ba41d2b85
cariosecus <cariosecus@gmail.com>
2019-10-17 17:41:37 +0100

    more tweaks

--- INSTALL.py
+++ INSTALL.py
@@ -17,7 +17,7 @@ os.system("sudo apt install make git unzip python3 python3-pip lib32z1 lib32ncur
 os.system("sudo apt autoremove")
 os.system("sudo apt autoclean")
 print("Installing BYOND...")
-exists = os.path.isfile(os.path.join(byond_version_major,'.',byond_version_minor,"_byond_linux.zip"))
+exists = os.path.isfile(os.path.join(mdir,cdir,byond_version_major,'.',byond_version_minor,"_byond_linux.zip"))
 if not exists:
 	os.system("sudo wget http://www.byond.com/download/build/{}/{}.{}_byond_linux.zip".format(byond_version_major,byond_version_major,byond_version_minor))
 os.system("unzip {}.{}_byond_linux.zip".format(byond_version_major,byond_version_minor))
