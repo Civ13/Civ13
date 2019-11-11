85955327fcf907d0d5dd45b4d97883a7f3ac350f
cariosecus <cariosecus@gmail.com>
2019-11-11 22:33:43 +0000

    arab town 2 WIP

--- scripts/mapswap.py
+++ scripts/mapswap.py
@@ -121,6 +121,8 @@ elif map == "KHALKHYN_GOL":
 	dmms.append("#include \"maps\\1943\\khalkhyn_gol.dmm\"")
 elif map == "ARAB_TOWN":
 	dmms.append("#include \"maps\\1969\\arab_town.dmm\"")
+elif map == "ARAB_TOWN_2":
+	dmms.append("#include \"maps\\1969\\arab_town_2.dmm\"")
 elif map == "GLADIATORS":
 	dmms.append("#include \"maps\\313bc\\gladiators.dmm\"")
 elif map == "OMAHA":
