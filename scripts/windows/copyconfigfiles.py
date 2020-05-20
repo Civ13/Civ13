fc4e09bf3e9e8e1a64024830c4a2834f7d0e14e4
Taislin <cariosecus@gmail.com>
2020-05-20 01:01:02 +0100

    windows scripts (untested)

---
+++ scripts/windows/copyconfigfiles.py
@@ -0,0 +1,26 @@
+import shutil
+import os
+
+currdir = os.path.dirname(os.path.abspath(__file__))
+lines = open(os.path.join(currdir,"paths.txt"))
+all_lines = lines.readlines()
+mdir = all_lines[1]
+mdir = mdir.replace("\n", "")
+mdir = mdir.replace("mdir:", "")
+cdir = all_lines[2]
+cdir = cdir.replace("\n", "")
+cdir = cdir.replace("cdir:", "")
+
+with open(os.path.join(mdir,cdir,"scripts/copiedconfigpaths.txt")) as lines:
+	for line in lines:
+		path = line.replace("\n", "")
+		shutil.copyfile(os.path.join(mdir,"civ13-git",path), os.path.join(mdir,cdir,path))
+
+with open(os.path.join(mdir,cdir,'scripts/copiedfolderpaths.txt')) as lines:
+	for line in lines:
+		path = line.replace("\n", "")
+		shutil.rmtree(os.path.join(mdir,cdir,path))
+		shutil.copytree(os.path.join(mdir,"civ13-git",path), os.path.join(mdir,cdir,path))
+
+shutil.rmtree(os.path.join(mdir,cdir,"config/names"))
+shutil.copytree(os.path.join(mdir,"civ13-git/config/names"), os.path.join(mdir,cdir,"config/names"), symlinks=False, ignore=None)
