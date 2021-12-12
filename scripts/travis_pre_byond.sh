5ba9efe6fc1ebdfd5aab4647e116d73db8ad1546
Taislin <cariosecus@gmail.com>
2021-12-12 23:29:19 +0000

    testing github actions

--- scripts/travis_pre_byond.sh
+++ scripts/travis_pre_byond.sh
@@ -2,18 +2,16 @@
 set -e
 
 echo "Setting up BYOND."
-rm -rf "$HOME/BYOND"
-mkdir -p "$HOME/BYOND"
-cd "$HOME/BYOND"
-curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+rm -rf "BYOND"
+mkdir -p "BYOND"
+cd "BYOND"
+curl "http://www.byond.com/download/build/514/514.1471_byond_linux.zip" -o byond.zip
 unzip byond.zip
 rm byond.zip
 cd byond
 make here
 echo "Compiling the dme..."
 retval=1
-source $HOME/BYOND/byond/bin/byondsetup
-cd /home/travis/build/Civ13/Civ13
 DreamMaker -max_errors 0 civ13.dme | tee result.log
 retval=$?
 if ! grep '\- 0 errors, 0 warnings' result.log
