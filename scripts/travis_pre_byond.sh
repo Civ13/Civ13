86cb54d0d14252e75968ebdc88dc2d2f827f6384
Taislin <cariosecus@gmail.com>
2021-12-12 23:35:02 +0000

    https

--- scripts/travis_pre_byond.sh
+++ scripts/travis_pre_byond.sh
@@ -5,7 +5,7 @@ echo "Setting up BYOND."
 rm -rf "BYOND"
 mkdir -p "BYOND"
 cd "BYOND"
-curl "http://www.byond.com/download/build/514/514.1471_byond_linux.zip" -o byond.zip
+curl "https://www.byond.com/download/build/514/514.1571_byond_linux.zip" -o byond.zip
 unzip -o byond.zip
 cd byond
 make here
